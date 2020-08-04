9e7447f9cd1239069e2c2269d3f0a57e26c89b26
helendhabte <helendhabte@gmail.com>
2020-08-03 17:28:50 -0700

    Update data cleaning.py
    Added more details to the welcome message & instructions section

--- data cleaning.py
+++ data cleaning.py
@@ -81,38 +81,53 @@ print(new_data)
 
 
 ##Welcome message & instructions  
-user_input = input("Welcome to the Data Cleaning Tool. Choose [1] Drop empty rows/columns [2] Drop Column [3] Drop Row [4] Change Column Name [5] Modify Single Cell [6] Compute Mean [7] Compute Median : ")
+user_input = input('''Welcome to the Data Cleaning Tool. Choose from the following optons:
+                  [1] Drop empty rows/columns 
+                  [2] Drop Column 
+                  [3] Drop Row 
+                  [4] Change Column Name 
+                  [5] Modify Single Cell 
+                  [6] Compute Mean 
+                  [7] Compute Median 
+                          Enter value: ''')
 
 if int(user_input) == 1:
-    param_input1 = input("Remove row(0) or column(1)? ")
-    param_input2 = input("Drop cells with all or any missing values? ")
+    param_input1 = input('''Remove Missing Values
+                         Drop a row(0) or a column(1)? ''')
+    param_input2 = input("Drop where all values are missing or if any value is missing?(Enter all or any): ")
     drop_na (int(param_input1), param_input2)
     print("Data Dropped")  
 elif int(user_input) == 2:
-    col_index = input("Enter the column index to drop: ")
+    col_index = input('''Drop Column
+                      Enter the column index to drop: ''')
     drop_col(int(col_index))
     print("Column Dropped")
 elif int(user_input) == 3:
-    row_index = input("Enter the row index to drop: ")
+    row_index = input('''Drop Row
+                      Enter the row index to drop: ''')
     drop_row(int(row_index))
     print("Row Dropped")
 elif int(user_input) == 4:
-    input1 = input("Enter column index to modify: ")
+    input1 = input('''Modify Column Name
+                   Enter column index to modify: ''')
     input2 = input("Enter new column name: ")
     col_name(int(input1), input2)
     print("Column Name Modified")
 elif int(user_input) == 5:
-    row_input = input("Row to modify: ")
+    row_input = input('''Modify Single Cell
+                      Row to modify: ''')
     col_input = input("Column to modify: ")
     newData_input = input("Input new data: ")
     cell_value(int(row_input), int(col_input), newData_input)
     print("Cell Modified")
 elif int(user_input) == 6:
-    column_name_input = input("Enter the name of the column: ")
+    column_name_input = input('''Compute Mean
+                              Enter the name of the column: ''')
     compute_mean(column_name_input)
     print("Cell(s) Filled - Mean Computed")
 elif int(user_input) == 7:
-    column_name_input = input("Enter the name of the column: ")
+    column_name_input = input('''Compute Median
+                              Enter the name of the column: ''')
     compute_median(column_name_input)
     print("Cell(s) Filled - Median Computed") 
 
